991b887bb1dab731e5a62afba9d61d3370c1b95c
xiaotian zhang <xiaotiannj@gmail.com>
2018-08-19 22:10:54 -0400

    clear and add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { RestaurantDetailModule } from './restaurant-detail/restaurant-detail.module';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: RestaurantsComponent }
+  { path: 'home', component: RestaurantsComponent },
+  { path: 'restaurant/:name', loadChildren: '../app/restaurant-detail/restaurant-detail.module#RestaurantDetailModule'}
 ];
 
 @NgModule({
